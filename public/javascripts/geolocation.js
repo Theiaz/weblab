88eb3927bc24bf302292772e50314146da41e4b0
Julian <julian_schaefer93@web.de>
2016-05-09 16:47:21 +0200

    added validator

--- public/javascripts/geolocation.js
+++ public/javascripts/geolocation.js
@@ -2,28 +2,69 @@
  * Created by Julian on 29.04.2016.
  */
 
-
+//Koordinaten
 var coordinates = {
     latitude : 0,
     longitude: 1,
     accuracy : 2
 }
 
-// var geoOptions = {
-//     enableHighAccuracy: true,
-//     timeout: 5000,
-//     maximumAge: 0
-// };
-//
-// function success(pos) {
-//     var crd = pos.coords;
-//     coordinates.latitude = crd.latitude;
-//     coordinates.longitude = crd.longitude;
-// };
-//
-// function error(err) {
-//     console.warn('ERROR(' + err.code + '): ' + err.message);
-// };
+// Validator Konstruktor
+function Validator(_class, logic) {
+    this.class = _class;
+    this.validate = logic;
+}
+
+// Validierungsfehler Konstruktor
+function Error(element, errorMessage) {
+    this.element = element;
+    this.errorMessage = errorMessage;
+}
+// Validierungslogik
+var locationValidator = new Validator("locationInput", function(element, errors) {
+    var value = element.value;
+    if (!/^\w+[\s\w+]*$/.test(value)) {
+        errors.push(new Error(element,
+            "Die Location muss aus Wörtern bestehen! \n"));
+    }
+});
+//erlaubt nur ein zusammenhängendes wort, welches mit # beginnt
+var hashValidator = new Validator("hashInput", function(element, errors) {
+    var value = element.value;
+    if (!/^#\S*$/.test(value)) {
+        errors.push(new Error(element,
+            "Der Hash muss mit '#' beginnen und aus Buchstaben bestehen! "));
+    }
+});
+
+// Validierungsfunktion
+var validate = function(event) {
+    //validiere einzelne einträge
+    var validators = this.validators;
+    var errors = [];
+    for (var i = 0; i < validators.length; i++) {
+        var validator = validators[i];
+        var elements = this.getElementsByClassName(validator.class);
+        for (var j = 0; j < elements.length; j++) {
+            validators[i].validate(elements[j], errors);
+        }
+    }
+    //falls error
+    if (errors.length > 0) {
+        var messages = errors.reduce(function(previousValue, currentValue) {
+            return previousValue + currentValue.errorMessage;
+        }, "");
+        alert(messages);
+        errors.forEach(function highlight(item) {
+            var classAttr = item.element.getAttribute("class");
+            classAttr += " error";
+            item.element.setAttribute("class", classAttr);
+        });
+        event.preventDefault(); // prevent default action
+        event.stopImmediatePropagation(); // prevent other handlers on same element
+        errors[0].element.focus();
+    }
+};
 
 function getGeoPosition() {
     if (navigator.geolocation) {
@@ -38,6 +79,8 @@ function getGeoPosition() {
         ausgabe.innerHTML = 'Ihr Browser unterstützt keine Geolocation.';
     }
 }
+
+
 
 // Formular per asynchronem Ajax Aufruf senden
 function sendAjaxAsync(event) {
@@ -87,6 +130,10 @@ function sendAjaxAsync(event) {
 
 
 $(document).ready(function() {
+    //Valideren
+    $("#geoForm").prop("validators", [ locationValidator, hashValidator ]);
+    $("#geoForm").bind("submit", validate);
+    //position ermitteln und Dokument updaten
     $("#geoForm").bind("submit", sendAjaxAsync);
 });
 
